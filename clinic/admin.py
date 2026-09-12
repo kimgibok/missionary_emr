a6a16bbef7725e2b8c8cd978cfff63e818e101c7
kimgibok <ruddjs33@naver.com>
2026-09-12 14:24:20 +0900

    clinic 앱: 처방/약품 도메인 모델(Drug, DrugBatch, MissionStock, Prescription, PrescriptionDrug)

--- clinic/admin.py
+++ clinic/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import Mission, Role, MissionMembership, Department, DrugCategory1, DrugCategory2, Patient, Visit, VisitDepartment
-
+from .models import Drug, DrugBatch, MissionStock, Prescription, PrescriptionDrug
 
 @admin.register(Mission)
 class MissionAdmin(admin.ModelAdmin):
@@ -49,3 +49,33 @@ class VisitAdmin(admin.ModelAdmin):
 class VisitDepartmentAdmin(admin.ModelAdmin):
     list_display = ('visit', 'user', 'assigned_at', 'is_done')
     list_filter = ('is_done',)
+    
+
+@admin.register(Drug)
+class DrugAdmin(admin.ModelAdmin):
+    list_display = ('name', 'cat1', 'cat2', 'ingredient')
+    list_filter = ('cat1', 'cat2')
+    search_fields = ('name', 'ingredient')
+
+
+@admin.register(DrugBatch)
+class DrugBatchAdmin(admin.ModelAdmin):
+    list_display = ('drug', 'source', 'received_date', 'expiry_date', 'total_qty', 'remaining_qty')
+    list_filter = ('source',)
+
+
+@admin.register(MissionStock)
+class MissionStockAdmin(admin.ModelAdmin):
+    list_display = ('drugbatch', 'mission', 'allocated_qty', 'remaining_qty', 'donated_qty', 'is_settled')
+    list_filter = ('mission', 'is_settled')
+
+
+@admin.register(Prescription)
+class PrescriptionAdmin(admin.ModelAdmin):
+    list_display = ('visit_department', 'department', 'is_dispensed')
+    list_filter = ('department', 'is_dispensed')
+
+
+@admin.register(PrescriptionDrug)
+class PrescriptionDrugAdmin(admin.ModelAdmin):
+    list_display = ('prescription', 'drug', 'dose_per_intake', 'frequency_per_day', 'duration_days', 'quantity')
